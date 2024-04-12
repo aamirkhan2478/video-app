09e24dc067e88f59c8941c9da79a4a22f046d26e
DarkenTech1234@ <aamir@darkentech.com>
2024-04-12 23:43:25 +0500

    first commit

---
+++ app/_layout.jsx
@@ -0,0 +1,12 @@
+import { Slot } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+const RootLayout = () => {
+  return (
+    <Slot/>
+  );
+};
+
+export default RootLayout;
+
+
